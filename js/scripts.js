261594f7ffa6ed12897f328a6598d6941b81b9ef
Evelynn M Tierney <74747292+Evemercy86@users.noreply.github.com>
2021-02-18 18:08:02 -0700

    Created list array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-alert('Hello world');
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+let pokemonList = [
+  { name: 'Eevee', height: 1, wieght:6.5, types: ['normal', 'fighting', 'evolution'] },
+  { name: 'Charmander', height:2, wieght:8.5, types: ['fire', 'blaze', 'charmeleon'] },
+  { name: 'Snorlax', height: 6.11, wieght:460, types: ['normal', 'fighting', 'immunity'] }
+  { name: 'Jigglypuff', height: 1.8, wieght:5.5, types: ['fairy', 'normal', 'poison', 'steel'] }
+  { name: 'Jynx', height: 4.7, wieght:40.6, types: ['ice', 'psychic', 'oblivious'] }
+];
