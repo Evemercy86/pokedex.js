24e99507b22e3cbefe877fbaf3e4f44dc16ffaf4
Evelynn M Tierney <74747292+Evemercy86@users.noreply.github.com>
2021-02-21 11:33:42 -0700

    corrections

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let pokemonList = [
-  { name: 'Eevee', height: 1, wieght:6.5, types: ['normal', 'fighting', 'evolution'] },
-  { name: 'Charmander', height:2, wieght:8.5, types: ['fire', 'blaze', 'charmeleon'] },
-  { name: 'Snorlax', height: 6.11, wieght:460, types: ['normal', 'fighting', 'immunity'] }
-  { name: 'Jigglypuff', height: 1.8, wieght:5.5, types: ['fairy', 'normal', 'poison', 'steel'] }
-  { name: 'Jynx', height: 4.7, wieght:40.6, types: ['ice', 'psychic', 'oblivious'] }
+  { name: 'Eevee', height: 1, weight:6.5, types: ['normal', 'fighting', 'evolution'] },
+  { name: 'Charmander', height:2, weight:8.5, types: ['fire', 'blaze', 'charmeleon'] },
+  { name: 'Snorlax', height: 6.11, weight:460, types: ['normal', 'fighting', 'immunity'] },
+  { name: 'Jigglypuff', height: 1.8, weight:5.5, types: ['fairy', 'normal', 'poison', 'steel'] },
+  { name: 'Jynx', height: 4.7, weight:40.6, types: ['ice', 'psychic', 'oblivious'] },
 ];
